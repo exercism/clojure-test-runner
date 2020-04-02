e7d9d5e21526beada82f904c9bc32ca704e9c076
porkostomus <porkostomus@mail.com>
2020-04-01 22:34:50 -0700

    generate json

--- src/exercism/clojure_test_runner.clj
+++ src/exercism/clojure_test_runner.clj
@@ -1,7 +1,17 @@
 (ns exercism.clojure-test-runner
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [cheshire.core :as json])
   (:gen-class))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(comment
+  (json/generate-string {:foo "bar" :baz 5}))
