f5faf70d0cf4d5326dd142ad392a490328375cba
porkostomus <porkostomus@mail.com>
2021-08-20 22:12:17 -0700

    load test file inside let form

--- src/clojure_test_runner.clj
+++ src/clojure_test_runner.clj
@@ -1,17 +1,14 @@
 (ns exercism.clojure-test-runner
   (:require
-   [clojure.test :refer [deftest testing is run-tests test-all-vars test-ns test-var]]
    [cheshire.core :as json]
    [kaocha.repl :as k]
-   [clojure.java.shell :as shell]
    [clojure.string :as str])
   (:gen-class))
 
 (defn -main [slug in-dir out-dir]
-  (let [ns (ns-publics (symbol (str slug "-test")))
+  (let [test-file (load-file (str in-dir (str/replace slug "-" "_") "_test.clj"))
+        ns (ns-publics (symbol (str slug "-test")))
         tests (keys ns)]
-    (shell/sh "cp" (str in-dir (str/replace slug "-" "_") ".clj") "src")
-    (shell/sh "cp" (str in-dir (str/replace slug "-" "_") "_test.clj") "test")
     (spit (str out-dir "results.json")
         (json/generate-string 
          (reduce (fn [m k]
@@ -19,4 +16,4 @@
             {} tests)))))
 
 (comment
-  (-main "two-fer" "resources/" "out-dir/"))
+  (-main "two-fer" "test/" "out-dir/"))
