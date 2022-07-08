8413969e8277906d030bfdb62d0bc472bf3dfa48
David Rolle <miridius@miridi.us>
2022-07-08 17:03:43 +0200

    Handle assertions which are not at top level
    - the whole top level form (one level below deftest) is shown for fails, which could contain multiple assertions
    - passing tests shown the whole test code

--- test-runner.clj
+++ test-runner.clj
@@ -29,40 +29,59 @@
   [loc]
   (= (symbol 'deftest) (-> loc z/down z/sexpr)))
 
+(defn assertion?
+  "Returns true if the given node is an `is`."
+  [loc]
+  (= (symbol 'is) (-> loc z/down z/sexpr)))
+
 (defn test-name
   "Returns the name of the test at a given node."
   [loc]
   (-> loc z/down z/right z/sexpr))
 
 (defn test-code
-  "Returns a vector of the code for all assertions in a given test."
+  "Returns all of the test code for a given test"
   [loc]
-  (->> loc z/down z/right z/right
-       (iterate z/right)
+  (->> (z/child-sexprs loc)
+       (drop 2)
+       (str/join "\n")))
+
+(defn find-assertions
+  "Returns a vector of the code for individual assertions in a given test."
+  [loc]
+  (->> (z/of-string (z/->string loc)) ;; isolate subtree to prevent find from leaving (is there a better way?)
+       (iterate #(z/find-next-depth-first % assertion?))
+       rest
        (take-while some?)
+       (map #(->> (iterate z/up %)
+                  (take-while (complement test?))
+                  last))
        (map z/sexpr)
        (mapv str)))
 
-(def tests
+(defn for-each-test
   "Traverses the zipper representing the parsed test file.
-   Contains a vector of the test names in the order defined."
-  (loop [loc zloc tests []]
-    (cond
-      (nil? loc) tests
-      (test? loc) (recur (z/right loc) (conj tests (test-name loc)))
-      :else (recur (z/right loc) tests))))
+   Returns the result of calling f on each test."
+  [f]
+  (->> (iterate z/right zloc)
+       (take-while some?)
+       (filter test?)
+       (map f)))
+
+(def tests
+  "A vector of the test names in the order defined."
+  (for-each-test test-name))
 
 (def test-codes
-  "Traverses the zipper representing the parsed test file.
-   Creates a vector of the test codes in the order defined."
-  (loop [loc zloc tests []]
-    (cond
-      (nil? loc) tests
-      (test? loc) (recur (z/right loc) (conj tests (test-code loc)))
-      :else (recur (z/right loc) tests))))
+  "A vector of the test codes in the order defined."
+  (for-each-test test-code))
 
-(def test-code-map
-  (zipmap tests test-codes))
+(def test-assertions
+  "A vector of vectors of test assertions in the order defined."
+  (for-each-test find-assertions))
+
+(def test-code-map (zipmap tests test-codes))
+(def test-assertions-map (zipmap tests test-assertions))
 
 ;; State to hold test results
 (def passes (atom []))
@@ -78,13 +97,12 @@
     "pass" nil))
 
 (defn get-test-code [test-name status]
-  (let [assertions (test-code-map test-name)
-        count ((swap! assertion-counts update test-name (fnil inc 0)) test-name)]
+  (let [a-count ((swap! assertion-counts update test-name (fnil inc 0)) test-name)]
     (if (= "pass" status)
-      ;; for passing tests show all assertions
-      (str/join "\n" assertions)
+      ;; for passing tests show the whole test
+      (test-code-map test-name)
       ;; for fails just the one that failed
-      (assertions (dec count)))))
+      ((test-assertions-map test-name) (dec a-count)))))
 
 (defn report [m status]
   (let [name (:name (meta (first t/*testing-vars*)))
---
+++ tests/example-nested-multiple-assertions/test/leap_test.clj
@@ -0,0 +1,194 @@
+(ns leap-test
+  (:require [clojure.test :refer [deftest is testing]]
+            leap))
+
+;; below tests are adapted from examples of nested tests found in the clojure track
+;; each has a case where all assertions pass, where one fails, and where one errors
+
+(deftest day-test-pass
+  (testing "numeric pattern for day matches"
+    (testing "un-padded 1"
+      (is (= "1" "1")))
+    (testing "un-padded 2"
+      (is (= "2" "2")))
+    (testing "un-padded 3"
+      (is (= "3" "3")))
+    (testing "un-padded 4"
+      (is (= "4" "4"))))
+  (testing "numeric pattern for day doesn't match"
+    (testing "too few digits"
+      (is (nil? nil)))
+    (testing "too many digits"
+      (is (nil? nil)))
+    (testing "one letter"
+      (is (nil? nil)))
+    (testing "two letters"
+      (is (nil? nil)))))
+
+(deftest day-test-fail
+  (testing "numeric pattern for day matches"
+    (testing "un-padded 1"
+      (is (= "1" "1")))
+    (testing "un-padded 2"
+      (is (= "2" "2")))
+    (testing "un-padded 3"
+      (is (= "3" "WRONG")))
+    (testing "un-padded 4"
+      (is (= "4" "4"))))
+  (testing "numeric pattern for day doesn't match"
+    (testing "too few digits"
+      (is (nil? nil)))
+    (testing "too many digits"
+      (is (nil? nil)))
+    (testing "one letter"
+      (is (nil? nil)))
+    (testing "two letters"
+      (is (nil? nil)))))
+
+(deftest day-test-error
+  (testing "numeric pattern for day matches"
+    (testing "un-padded 1"
+      (is (= "1" "1")))
+    (testing "un-padded 2"
+      (is (= "2" "2")))
+    (testing "un-padded 3"
+      (is (= "3" "3")))
+    (testing "un-padded 4"
+      (is (= "4" "4"))))
+  (testing "numeric pattern for day doesn't match"
+    (testing "too few digits"
+      (is (nil? nil)))
+    (testing "too many digits"
+      (is (nil? nil)))
+    (testing "one letter"
+      (is (nil? (case :oh-no!))))
+    (testing "two letters"
+      (is (nil? nil)))))
+
+
+(deftest day-names-test-pass
+  (testing "day names match"
+    (is (= "Sunday" "Sunday"))
+    (is (= "Monday" "Monday"))
+    (is (= "Tuesday" "Tuesday"))
+    (is (= "Wednesday" "Wednesday"))
+    (is (= "Thursday" "Thursday"))
+    (is (= "Friday" "Friday"))
+    (is (= "Saturday" "Saturday")))
+  (testing "day names don't match"
+    (testing "combined"
+      (is (nil? nil)))
+    (testing "short name"
+      (is (nil? nil)))
+    (testing "numeric day of the week (0-indexed)"
+      (is (nil? nil)))
+    (testing "numeric day of the week (1-indexed)"
+      (is (nil? nil)))))
+
+(deftest day-names-test-fail
+  (testing "day names match"
+    (is (= "Sunday" "Sunday"))
+    (is (= "Monday" "Monday"))
+    (is (= "Tuesday" "Tuesday"))
+    (is (= "Wednesday" "Wednesday"))
+    (is (= "Thursday" "Thursday"))
+    (is (= "Friday" "Friday"))
+    (is (= "Saturday" "Saturday")))
+  (testing "day names don't match"
+    (testing "combined"
+      (is (nil? false)))
+    (testing "short name"
+      (is (nil? nil)))
+    (testing "numeric day of the week (0-indexed)"
+      (is (nil? nil)))
+    (testing "numeric day of the week (1-indexed)"
+      (is (nil? nil)))))
+
+(deftest day-names-test-error
+  (testing "day names match"
+    (is (= "Sunday" "Sunday"))
+    (is (= "Monday" "Monday"))
+    (is (= "Tuesday" "Tuesday"))
+    (is (= "Wednesday" "Wednesday"))
+    (is (= "Thursday" "Thursday"))
+    (is (= "Friday" "Friday"))
+    (is (= "Saturday" (assert nil))))
+  (testing "day names don't match"
+    (testing "combined"
+      (is (nil? nil)))
+    (testing "short name"
+      (is (nil? nil)))
+    (testing "numeric day of the week (0-indexed)"
+      (is (nil? nil)))
+    (testing "numeric day of the week (1-indexed)"
+      (is (nil? nil)))))
+
+(deftest increment-decrement-and-get-balance-pass
+  (testing "Taking money out of the account works"
+    (let [account (atom 0)]
+      (is (= 0 @account))
+      (swap! account + 10)
+      (is (= 10 @account))
+      (swap! account + -10)
+      (is (= 0 @account)))))
+
+(deftest increment-decrement-and-get-balance-fail
+  (testing "Taking money out of the account works"
+    (let [account (atom 0)]
+      (is (= 0 @account))
+      (swap! account + 10)
+      (is (= 1000 @account))
+      (swap! account + -10)
+      (is (= 0 @account)))))
+
+(deftest increment-decrement-and-get-balance-error
+  (testing "Taking money out of the account works"
+    (let [account (atom 0)]
+      (is (= 0 @account))
+      (swap! account + 10)
+      (is (= 10 @@account))
+      (swap! account + -10)
+      (is (= 0 @account)))))
+
+(deftest largest-series-tests-pass
+  (testing "can find the largest product of 2 with numbers in order"
+    (is (= 72 72)))
+  (testing "can find the largest product of 2"
+    (is (= 48 48)))
+  (testing "finds the largest product if span equals length"
+    (is (= 18 18))))
+
+(deftest largest-series-tests-fail
+  (testing "can find the largest product of 2 with numbers in order"
+    (is (= 72 "WRONG")))
+  (testing "can find the largest product of 2"
+    (is (= 48 48)))
+  (testing "finds the largest product if span equals length"
+    (is (= 18 18))))
+
+(deftest largest-series-tests-error
+  (testing "can find the largest product of 2 with numbers in order"
+    (is (= 72 72)))
+  (testing "can find the largest product of 2"
+    (is (= 48 48)))
+  (testing "finds the largest product if span equals length"
+    (is (= 18 (+ :foo)))))
+
+
+(deftest is-inside-let-pass
+  (let [v [1 2 3 4]]
+    (is (= 4 (count v)))
+    (is (sequential? v))
+    (is (= 2 (second v)))))
+
+(deftest is-inside-let-fail
+  (let [v (range 4)]
+    (is (= 4 (count v)))
+    (is (sequential? v))
+    (is (= 2 (second v)))))
+
+(deftest is-inside-let-error
+  (let [v 4]
+    (is (= 4 (count v)))
+    (is (sequential? v))
+    (is (= 2 (second v)))))
