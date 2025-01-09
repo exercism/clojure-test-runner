4e6fd2e49c9535f0a5142ad08c1f32c344a22d60
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-09 09:36:47 +0100

    Convert to new structure

--- tests/all-fail/src/leap.clj
+++ tests/all-fail/src/leap.clj
@@ -3,6 +3,6 @@
 (defn leap-year? [year]
   (let [[p c s] (map #(= 0 (rem year %1)) [4 100 400])]
     (not
-      (or (and p (not c))
-          (and c s)))))
+     (or (and p (not c))
+         (and c s)))))
     
--- tests/partial-fail/test/leap_test.clj
+++ tests/partial-fail/test/leap_test.clj
@@ -1,30 +1,16 @@
 (ns leap-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             leap))
 
-(deftest year-not-divisible-by-4
-  (is (not (leap/leap-year? 2015))))
+(deftest leap-year?
+  (testing "year divisible by 4 and 5 is still a leap year"
+    (is (leap/leap-year? 1960)))
 
-(deftest year-divisible-by-2-but-not-4
-  (is (not (leap/leap-year? 1970))))
+  (testing "year not divisible by 4 in common year"
+    (is (not (leap/leap-year? 2015))))
 
-(deftest year-divisible-by-4-but-not-100
-  (is (leap/leap-year? 1996)))
+  (testing "year divisible by 400 is leap year"
+    (is (leap/leap-year? 1970)))
 
-(deftest year-divisible-by-4-and-5
-  (is (leap/leap-year? 1960)))
-
-(deftest year-divisible-by-100-but-not-400
-  (is (not (leap/leap-year? 2100))))
-
-(deftest year-divisible-by-100-but-not-by-3
-  (is (not (leap/leap-year? 1900))))
-
-(deftest year-divisible-by-400
-  (is (leap/leap-year? 2000)))
-
-(deftest year-divisible-by-400-but-not-125
-  (is (leap/leap-year? 2400)))
-
-(deftest year-divisible-by-200-but-not-by-400
-  (is (not (leap/leap-year? 1800))))
+  (testing "year divisible by 200, not divisible by 400 in common year"
+    (is (not (leap/leap-year? 1800)))))
